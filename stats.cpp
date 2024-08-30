b338496b1503e39e58fb6a5c8eeda3298f1d9b92
medhawee9 <141835041+medhawee9@users.noreply.github.com>
2024-08-30 15:23:04 +0530

    Update stats.cpp
    Implemented statistics

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,19 @@
 #include "stats.h"
+#include <algorithm>
+#include <limits>
+#include <numeric>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Stats Statistics::ComputeStatistics(const std::vector<float>& numbers) {
+    Stats stats;
+
+    if (numbers.empty()) {
+        stats.average = std::numeric_limits<float>::quiet_NaN();
+        stats.min = std::numeric_limits<float>::quiet_NaN();
+        stats.max = std::numeric_limits<float>::quiet_NaN();
+    } else {
+        stats.average = std::accumulate(numbers.begin(), numbers.end(), 0.0f) / numbers.size();
+        stats.min = *std::min_element(numbers.begin(), numbers.end());
+        stats.max = *std::max_element(numbers.begin(), numbers.end());
+    }
+     return stats;
 }
